0cfa9f18943c163fdc47bc2364e3a2de2ffdb1c1
Abby Smith <acsmith25@crimson.ua.edu>
2018-08-23 15:47:20 -0500

    - public test function

---
+++ Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  TestModuleKit
+//
+//  Created by Abby Smith on 8/23/18.
+//  Copyright © 2018 Abby Smith. All rights reserved.
+//
+
+import Foundation
+
+public func getName() -> String {
+    return "Abby Smith"
+}
---
+++ TestModuleKit/TestModuleKit.h
@@ -0,0 +1,19 @@
+//
+//  TestModuleKit.h
+//  TestModuleKit
+//
+//  Created by Abby Smith on 8/23/18.
+//  Copyright © 2018 Abby Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestModuleKit.
+FOUNDATION_EXPORT double TestModuleKitVersionNumber;
+
+//! Project version string for TestModuleKit.
+FOUNDATION_EXPORT const unsigned char TestModuleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestModuleKit/PublicHeader.h>
+
+
